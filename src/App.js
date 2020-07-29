e0da07f16be3b9918c6e7855557015df555443a4
Trevor <alutiiq@comcast.net>
2020-07-29 11:56:17 -0700

    almost working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+	constructor (){
+		super()
+
+		this.state = {
+			dropDownVisible: false,
+			clockwise: true
+		}
+	}
+
+	toggleDropDown = () => {
+		this.setState({
+			dropDownVisible: !this.state.dropDownVisible 
+		})
+	}
+
+	toggleSpin = () => {
+		this.setState({
+			clockwise: !this.state.clockwise
+		})
+	}
+	
 	render() {
 		return (
 			<div className="App">
@@ -11,14 +32,29 @@ class App extends Component {
 						src="https://v.fastcdn.co/u/420f55dd/28913912-0-dm-white-logo-2.png"
 						alt="DevMountain"
 					/>
-					<nav>
+					<nav className="desktop-menu">
 						<span>Home</span>
 						<span>About</span>
 						<span>Contact</span>
 					</nav>
+					<div className="dropdown" onClick={this.toggleDropDown}>Menu</div>
+					{
+						this.state.dropDown ? (
+					<nav className="mobile-menu">
+						<span>Home</span>
+						<span>About</span>
+						<span>Contact</span>
+					</nav>)
+		 			: null
+					}
 				</header>
 				<section className="animation-transition">
-					<img className="react-logo" src={logo} alt="React.js" />
+					<img 
+						className={this.state.clockwise ? "react-logo spin-clockwise" : "react-logo spin-counterClockwise"}
+					 src={logo} 
+					 alt="React.js"
+					onClick = {this.toggleSpin}
+					/>
 					<div className="transition-circle">Hover your mouse over me!</div>
 				</section>
 			</div>
